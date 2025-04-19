98c0f49614254803f7074f44c3b30cb2c1d7181b
Nick <nicollaslucena1505@gmail.com>
2025-04-19 14:05:55 -0300

    fix commit

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 import os
 
-def exibir_opcoes():
-    print('1. Create new activity')
-    print('2. List activities')
+def show_menu():
+    print('1. Create new task')
+    print('2. List tasks')
     print('3. Leave\n')
+
+def show_subtitle(texto):
+    os.system('cls')
+    linha = '*' * (len(texto))
+    print(linha)
+    print(texto)
+    print(linha)
+    print()
+
+def leave_program():
+    show_subtitle('...finishing...')
+
+
+def create_task():
+    task_name = input("What's your task's title?: ")
+    task_desc = input("Describe your task: ")
+    task_time = input("How long time you'll take to complete your task?: ")
+    task_data = {'title': task_name, 'description': task_desc, 'time to complete': task_time}
+    tasks.append(task_data)
+    print(f'The task {task_name} was succesfully registred!')
+    back_menu()
 
 def back_menu():
     input('\nPress any key for back to the menu...')
@@ -13,13 +34,37 @@ def invalid_option():
     print('Please insert a valid option!\n')
     back_menu()
 
-def list_activities():
-    pass
+def list_tasks():
+    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
+    for task in tasks:
+        task_name = task['title']
+        task_description = task['description']
+        task_time = task['time']
+        print(f'- {task_name.ljust(20)} | {task_description.ljust(20)} | {task_time}')
+
+    back_menu()
+
+def choose_one():
+    try:
+        choosen = int(input('Choose one option: '))
+        # choosen = int(choosen)
+
+        if choosen == 1: 
+            create_task()
+        elif choosen == 2: 
+            list_tasks()
+        elif choosen == 3: 
+            leave_program()
+        else: 
+            invalid_option()
+    except:
+        invalid_option()
+
 
 def main():
     os.system('cls')
     #exibir_nome_programa()
-    exibir_opcoes()
+    show_menu()
     #escolher_opcao()
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,22 +1,22 @@
 import os
 
-class Activity:
-    activities = []
+class Task:
+    tasks = []
 
     def __init__(self, tittle, description, time):
-        '''construtor para a atividade'''
+        '''construtor para a task'''
         self._title = tittle.title()
         self._description = description.upper()
         self._time = []
-        Activity.activities.append(self)
+        Task.tasks.append(self)
 
     def __str__(self):
-        '''Funcao para exibir o nome e a descricao da atividade'''
+        '''Funcao para exibir o nome e a descricao da tarefa'''
         return f'{self._title} | {self._description} | {self._time}'
 
     @classmethod
     def list_activities(cls):
-        '''Funcao para listar as atividades'''
-        print(f'{'Activity'.ljust(25)} | {'Description'.ljust(25)} | {'Time'.ljust(25)}')
-        for activity in cls.activities:
-            print(f'{activities._title.ljust(25)} | {activities._description.ljust(25)} | {str(activities.time).ljust(25)}')
+        '''Funcao para listar as tarefas'''
+        print(f'{'Task'.ljust(25)} | {'Description'.ljust(25)} | {'Time'.ljust(25)}')
+        for task in cls.tasks:
+            print(f'{tasks._title.ljust(25)} | {tasks._description.ljust(25)} | {str(tasks.time).ljust(25)}')
