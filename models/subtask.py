cbb14c165eaa880f156a85f5dc42b8e037be6341
Nick <nicollaslucena1505@gmail.com>
2025-04-29 15:50:51 -0300

    fix commmit: function 'editing_act'e rename 'app' to 'main'

---
+++ models/subtask.py
@@ -0,0 +1,23 @@
+import os
+from models.task import Task
+
+class Subtask(Task):
+    subtasks = []
+
+    def __init__(self, tittle, description, time):
+        '''construtor para a task'''
+        super().self.__init__(tittle)
+        self._description = description.upper()
+        self._time = []
+        Subtask.subtasks.append(self)
+
+    def __str__(self):
+        '''Funcao para exibir o nome e a descricao da subtask'''
+        return f'{self._description} | {self._time}'
+
+    @classmethod
+    def list_subtasks(clear):
+        '''Funcao para listar as subtasks'''
+        print(f'{'title'.ljust(25)} | {'description'.ljust(25)} | {'time to complete'.ljust(25)}')
+        for subtask in clear.subtasks:
+            print(f'{subtask._title.ljust(25)} | {subtask._description.ljust(25)} | {str(subtask.time).ljust(25)}')
--- models/task.py
+++ models/task.py
@@ -1,6 +1,7 @@
 import os
+from abc import ABC, abstractmethod
 
-class Task:
+class Task(ABC):
     tasks = []
 
     def __init__(self, tittle, description, time):
@@ -11,11 +12,11 @@ class Task:
         Task.tasks.append(self)
 
     def __str__(self):
-        '''Funcao para exibir o nome e a descricao da tarefa'''
+        '''Funcao para exibir o nome e a descricao da task'''
         return f'{self._title} | {self._description} | {self._time}'
 
     @classmethod
-    def list_activities(clear):
+    def list_tasks(clear):
         '''Funcao para listar as tarefas'''
         print(f'{'title'.ljust(25)} | {'description'.ljust(25)} | {'time to complete'.ljust(25)}')
         for task in clear.tasks:
